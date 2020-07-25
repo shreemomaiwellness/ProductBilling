388a97019ae2e8da644294e9c2650970ae2aa1c4
Bhanderi Meet <55754435+meetbhanderi@users.noreply.github.com>
2020-07-25 08:33:41 +0530

    initial commit

---
+++ ShreeMomaiWellnessBilling/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Shree Momai Wellness Billing Portal\ShreeMomaiWellnessBilling\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
